d010b7616ef52fa6d0fb8b99d9ddeee6d2a71441
poipiii <nanosonico1@gmail.com>
2023-05-05 23:19:29 +0800

    updated partner logo

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -10,22 +10,10 @@ const Partners = () => {
             <section id="About" className="About container">
                 <h1>Our partners.</h1>
                 <div className="partnerLogo">
-                    <Link href="https://www.avermedia.com/">
-                        <img src="/AVerMedia_logo_logotipo.png"  alt="" />
-
-                    </Link>
+                        <img src="/LOGOs.png"  alt="" />
 
                 </div>
-                   <div className="partnerLogoxp">
-                    <Link href="https://www.xp-pen.com/">
-                    <img src="/xp_pen_logo-2-new.png"   alt="" />
-                    </Link>
-                </div>
-                    <div className="partnerLogoxp">
-                    <Link href="https://www.xencelabs.com">
-                    <img src="/e9703f5c-37b3-4fec-8d00-e2e1731502bc.jpg"   alt="" />
-                    </Link>
-                </div>
+               
             </section>
         </main>
        <Footers></Footers>
